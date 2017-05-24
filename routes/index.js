a413593471941c062819414dd2920f0aff85791d
edplo <edplatomail@gmail.com>
2017-05-24 17:30:39 -0400

    changed api to the movie db, major data revisions for all views

--- routes/index.js
+++ routes/index.js
@@ -12,20 +12,20 @@ router.get('/', function(req, res, next) {
     if(!req.session.pool){
          var pool = new Pool(req.session.pool = {});
     }
-    res.render('index', { title: 'Flick Pick', data: data, movies: docs });
+    res.render('index', { data: data, movies: docs });
     });
 });
 
 router.get('/results', function(req, res, next) {
     var searchResult = req.query.search;
-    var url = "http://www.omdbapi.com/?s=" + searchResult;
+    var url = "https://api.themoviedb.org/3/search/movie?api_key=APIKEY&query=" + searchResult;
     request(url, function(error, response, body){
         if(!error && response.statusCode == 200) {
          var parsedbody = JSON.parse(body);
-         if(!parsedbody["Response"] || parsedbody["Response"] === "False"){
+         if(!parsedbody["total_results"] || parsedbody["total_results"] <= 0){
             res.redirect("/");
          } else {
-            res.render("index", { title: 'Flick Pick', data: parsedbody });
+            res.render("index", { data: parsedbody });
          }
         }
     });
@@ -33,21 +33,19 @@ router.get('/results', function(req, res, next) {
 
 router.get('/add-to-moviepool/:id', function(req, res, next) {
     var movieId = req.params.id;
-    var url = "http://www.omdbapi.com/?i=" + movieId;
+    var url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=APIKEY";
     var errMsg = req.flash('error')[0];
-
     request(url, function(error, response, body){
             if(!error && response.statusCode == 200) {
              var parsedbody = JSON.parse(body);
-             if(error || !parsedbody["Response"] || parsedbody["Response"] === "False"){
+             if(error || parsedbody["status_code"] === 34){
                 req.flash('error', 'Error storing movie onto database. Try again later.');
                 res.redirect("/");
              } else {
-      
     var pool = new Pool(req.session.pool ? req.session.pool : {});
     var movies = [
         new Movie({ 
-            imdbID: movieId,
+            id: movieId,
             detailBody: parsedbody
         })
     ];
